cdb05c034905a5f45190b462df05a94e3ba33efc
Qreamville <Oyeleyebalikis@yahoo.com>
2023-10-19 14:24:13 +0100

    feat: update

---
+++ constants/random.ts
@@ -0,0 +1,7 @@
+export const homeCategories = [
+  { name: "Furniture", src: "/sofa.png", color: "#f0d1ff" },
+  { name: "Clock", src: "/clock.png", color: "#ffd9ff" },
+  { name: "Lighting", src: "/light.png", color: "#bfd9d8" },
+  { name: "Beddings", src: "/bed.png", color: "#d2ffd4" },
+  { name: "Accessories", src: "/plant.png", color: "#fff6c1" },
+];
