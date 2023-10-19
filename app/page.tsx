d611684566b278a679e6e3f480aa1e490797698e
Qreamville <Oyeleyebalikis@yahoo.com>
2023-10-19 14:23:45 +0100

    feat: update app

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <></>;
+  return (
+    <main className="home">
+      <div className="hero">
+        <Image src={"/hero.jpg"} fill alt="hero" />
+      </div>
+      <section>
+        <h1>Our Products</h1>
+        <div></div>
+      </section>
+    </main>
+  );
 }
