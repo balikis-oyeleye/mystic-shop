8db309de57547b955e6604c32330a62e21a003ca
Qreamville <Oyeleyebalikis@yahoo.com>
2023-10-25 13:34:41 +0100

    remove search slice

--- redux/store.ts
+++ redux/store.ts
@@ -1,13 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import sidebarReducer from "./features/sidebarSlice";
-import searchReducer from "./features/searchSlice";
 import cartModalReducer from "./features/cartModalSlice";
 import contactModalSlice from "./features/contactModalSlice";
 
 export const store = configureStore({
   reducer: {
     sidebarReducer,
-    searchReducer,
     cartModalReducer,
     contactModalSlice,
   },
