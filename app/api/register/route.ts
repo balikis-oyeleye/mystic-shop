7a5405e80207dc3f27a89f84958c0945b85ab403
Qreamville <Oyeleyebalikis@yahoo.com>
2023-10-26 20:32:42 +0100

    fix: error

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -10,15 +10,15 @@ export async function POST(req: Request) {
   const hashedPassword = await bcrypt.hash(password, 12);
 
   try {
-    // const user = await prisma?.customer.create({
-    //   data: {
-    //     email,
-    //     name: fullName,
-    //     password: hashedPassword,
-    //   },
-    // });
+    const user = await prisma?.customer.create({
+      data: {
+        email,
+        name: fullName,
+        password: hashedPassword,
+      },
+    });
 
-    return NextResponse.json(body);
+    return NextResponse.json(user);
   } catch (e: any) {
     if (e instanceof Prisma.PrismaClientKnownRequestError) {
       if (e.code === "P2002") {
