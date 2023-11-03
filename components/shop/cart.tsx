3f869ce937d18678d8da33b23618b8b0b04a0701
Qreamville <Oyeleyebalikis@yahoo.com>
2023-11-03 16:05:51 +0100

    build: fix vercel

--- components/shop/cart.tsx
+++ components/shop/cart.tsx
@@ -1,11 +1,8 @@
 import { getCart } from "@/actions/getCart";
 import CartClient from "./cartClient";
-import { cookies } from "next/headers";
 
 const Cart = async () => {
-  const cookieStore = cookies();
-  const id = cookieStore.get("ms-id")?.value;
-  const cart = await getCart(id as string);
+  const cart = await getCart();
 
   return <CartClient cart={cart} />;
 };
