5ab370dffd0f91a9144444a3cf20a340e534fcce
Qreamville <Oyeleyebalikis@yahoo.com>
2023-11-08 08:57:48 +0100

    feat: update cart product

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -6,8 +6,9 @@ import { getCart } from "@/actions/getCart";
 
 export async function POST(request: Request) {
   const cookieStore = cookies();
-
+  const allCart = await getCart();
   const customer = await getCustomer();
+
   let id = cookieStore.get("ms-id")?.value;
 
   if (!id) return NextResponse.json({ message: "success" });
@@ -20,22 +21,43 @@ export async function POST(request: Request) {
     },
   });
 
-  const cart = await prisma.cart.updateMany({
-    where: {
-      customerId: id,
-    },
-    data: {
-      customerId: {
-        set: customer?.id,
-      },
-    },
+  cartProducts.forEach(async (item) => {
+    const isContained = allCart.some(
+      (prod) => prod.productId === item.productId
+    );
+
+    if (isContained) {
+      await prisma.cart.updateMany({
+        where: {
+          customerId: customer?.id,
+          productId: item.productId,
+        },
+        data: {
+          quantity: {
+            increment: 1,
+          },
+        },
+      });
+
+      await prisma.cart.delete({
+        where: {
+          id: item.id,
+        },
+      });
+    } else {
+      await prisma.cart.updateMany({
+        where: {
+          customerId: id,
+        },
+        data: {
+          customerId: {
+            set: customer?.id,
+          },
+        },
+      });
+    }
   });
 
   cookieStore.delete("ms-id");
-
-  if (cart) {
-    return NextResponse.json(cart);
-  } else {
-    NextResponse.error();
-  }
+  return NextResponse.json({ message: "success" });
 }
