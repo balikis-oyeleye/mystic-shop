97ad6ff68f274d271e93b9c07786656f96aa94d5
Qreamville <Oyeleyebalikis@yahoo.com>
2023-11-13 16:04:53 +0100

    feat: update

--- components/orderSummary.tsx
+++ components/orderSummary.tsx
@@ -2,7 +2,9 @@
 
 import axios from "axios";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
+import toast from "react-hot-toast";
 
 interface OrderProps {
   cart: {
@@ -24,6 +26,7 @@ interface OrderProps {
 }
 
 const OrderSummary = ({ cart }: OrderProps) => {
+  const router = useRouter();
   const [isLoading, setLoading] = useState(false);
   const sumTotal = cart.reduce(
     (sum, item) => sum + item.product.price * item.quantity,
@@ -39,7 +42,9 @@ const OrderSummary = ({ cart }: OrderProps) => {
         window.location = response.data.url;
       })
       .catch((error) => {
-        console.log(error);
+        if (error.response.data === "Unauthorized")
+          return router.push("/login");
+        else return toast.error("Something went wrong");
       })
       .finally(() => setLoading(false));
   };
